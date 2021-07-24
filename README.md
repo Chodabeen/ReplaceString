443aa31f2eef584fb548d9b7a61b2f4d7c4e6ae6
Chodabeen <22000711@handong.edu>
2021-07-24 15:49:00 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # ReplaceString
+
+# H08 -  문자열 바꾸기
+사용자에게 문자열과 찾을 문자열, 바꿀 문자열을 입력 받아서 원본 문자열에서 찾을 문자열을 찾을 후 바꿀 문자열로 변경하도록 한다. 이때 몇 번 변경되었는지 출력하도록 한다. 프로그램은 입력받는 문자열에 x가 들어오면 종료되도록 한다.
+
+변수는 다음과 같이 사용하라.
+
+String input; // 입력받는 문자열
+
+String findStr; // 찾을 문자열
+
+String replaceStr; // 바꾸고자 하는 문자열
+
+int i; // 반복문에 사용되는 변수
+
+실행예시
+```
+===============================
+=> Enter a sentence ('x' for exit) > It is more embarrassing not to trust a frind than to be deceived by a frind.
+=> Enter a sentence to find > frind
+=> Enter a sentence to replace > friend
+=> The replacement occurred 2 times
+=> It is more embarrassing not to trust a friend than to be deceived by a friend.
+===============================
+=> Enter a sentence ('x' for exit) > abc abc abc wef wef wef
+=> Enter a sentence to find > abc
+=> Enter a sentence to replace > xyz
+=> The replacement occurred 3 times
+=> xyz xyz xyz wef wef wef
+===============================
+=> Enter a sentence ('x' for exit) > x
+Exit program
+```
---
+++ src/edu/handong/csee/java/H08/Main.java
@@ -0,0 +1,49 @@
+package edu.handong.csee.java.H08;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Main stepH = new Main();
+        stepH.h08();
+    }
+
+    void h08() {
+      Scanner s = new Scanner(System.in);
+      String input;
+      String findStr;
+      String replaceStr;
+      int i = 0;
+      int check = 0;
+
+      while(true){
+        check = 0;
+        i = 0;
+        System.out.println("===============================");
+        System.out.print("=> Enter a sentence ('x' for exit) > ");
+        input = s.nextLine();
+
+        if(input.equals("x"))
+          break;
+          
+        System.out.print("=> Enter a sentence to find > ");
+        findStr = s.next();
+        System.out.print("=> Enter a sentence to replace > ");
+        replaceStr = s.next();
+
+        while(true){
+          check = input.indexOf(findStr, check);
+          input = input.replaceFirst(findStr, replaceStr);
+          if(check == -1)
+            break;
+          i++;
+        }
+
+        System.out.println("=> The replacement occurred " + i + " times");
+        System.out.println("=> " + input);
+        s.nextLine();
+        
+      }
+
+      System.out.println("Exit program");
+	}
+}
